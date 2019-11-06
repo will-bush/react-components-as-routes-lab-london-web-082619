3caa44d84d98ce02e624d9359a3d4cc64694f38e
Will Bush <williamdcbush@gmail.com>
2019-11-06 12:57:22 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => 
+        <div>
+          <p>Title: {movie.title}</p>
+          <p>Runtime: {movie.time}</p>
+          <ul>Genres:
+          {movie.genres.map(genre => <li>{genre}</li>)}
+          </ul>
+        </div>
+          )}
     </div>
   );
 };
